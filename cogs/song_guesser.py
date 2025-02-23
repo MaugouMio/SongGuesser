2a1b02526fb64d5d7d7a587d28d3fb142ae9ffcc
MaugouMio <maugoumine@gmail.com>
2025-02-23 17:14:26 +0800

    遊戲開始與片段播放流程

--- cogs/song_guesser.py
+++ cogs/song_guesser.py
@@ -1,7 +1,7 @@
 # This example requires the 'message_content' privileged intent to function.
 
 import asyncio
-import json
+import json, os, random
 
 import discord
 from youtube_dl import youtube_dl
@@ -15,7 +15,7 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 
 ytdl_format_options = {
 	'format': 'bestaudio/best',
-	'outtmpl': 'temp/main',
+	# 'outtmpl': 'temp/main',  # set individually for each discord guild
 	'restrictfilenames': True,
 	'noplaylist': True,
 	'nocheckcertificate': True,
@@ -32,7 +32,7 @@ ffmpeg_options = {
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
+ytdl_instances = dict()
 class YTDLSource(discord.PCMVolumeTransformer):
 	def __init__(self, source, *, data, volume=0.5):
 		super().__init__(source, volume)
@@ -43,17 +43,33 @@ class YTDLSource(discord.PCMVolumeTransformer):
 		self.url = data.get('url')
 
 	@classmethod
-	async def from_url(cls, url, *, loop=None, stream=False):
+	async def load_from_url(cls, url, parts, guild_id, *, loop=None):
+		directory = f"temp/{guild_id}"
+		if guild_id not in ytdl_instances:
+			if not os.path.exists(directory):
+				os.mkdir(directory)
+				
+			options = ytdl_format_options.copy()
+			options["outtmpl"] = f"{directory}/main"
+			ytdl_instances[guild_id] = youtube_dl.YoutubeDL(options)
+			
+		ytdl = ytdl_instances[guild_id]
 		loop = loop or asyncio.get_event_loop()
-		data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+		data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=True))
 
 		if 'entries' in data:
 			# take first item from a playlist
 			data = data['entries'][0]
 			
-		song = AudioSegment.from_file("temp/main", "m4a")
+		filename = f"{directory}/main"
+		song = AudioSegment.from_file(filename, "m4a")
+		for i in range(len(parts)):
+			part = parts[i]
+			song[part[0]:part[1]].export(f"{directory}/{i}", format="m4a")
 
-		filename = data['url'] if stream else ytdl.prepare_filename(data)
+	@classmethod
+	async def get_part(cls, part, guild_id):
+		filename = f"temp/{guild_id}/{part}"
 		return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
@@ -64,10 +80,21 @@ class GameStep:
 	PLAYING = 2		# 出完題目正在等待玩家回答的狀態
 
 class GameData:
-	def __init__(self):
+	def __init__(self, guild_id, text_channel):
+		self.guild_id = guild_id
+		self.text_channel = text_channel
+		
 		self.step = GameStep.IDLE
 		self.channel = None
 		self.question_set = None
+		self.current_question_idx = 0
+		self.current_question_part = 0
+	
+	def reset_progress(self):
+		self.current_question_idx = 0
+		self.current_question_part = 0
+		if self.question_set:
+			random.shuffle(self.question_set["questions"])
 	
 	@staticmethod
 	def is_valid_question_set(question_set):
@@ -78,17 +105,37 @@ class SongGuesser(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.games = dict()	 # <Guild, GameData>
+	
+	async def on_play_finished(self, e, game, message):
+		if e:
+			print(f"[{game.guild_id}] Play question {game.current_question_idx + 1} part {game.current_question_part + 1} error: {e}")
+		asyncio.run(message.edit(f"第 {game.current_question_idx + 1} 題片段 {game.current_question_part + 1} 播放完畢，快用 `/猜` 指令搶答吧！"))
+		
+	async def play_part(self, game):
+		source = await YTDLSource.get_part(game.current_question_part, game.guild_id)
+		message = await game.text_channel.send(f"正在播放第 {game.current_question_idx + 1} 題片段 {game.current_question_part + 1}，使用 `/猜` 指令進行搶答")
+		await asyncio.sleep(2)
+		voice_client.play(source, after=lambda e, game=game, message=message: asyncio.get_running_loop().create_task(self.on_play_finished(e, game, message)))
+	
+	async def init_question(self, game):
+		idx = game.current_question_idx
+		await YTDLSource.load_from_url(game.question_set["questions"][idx]["url"], game.question_set["questions"][idx]["parts"], game.guild_id, loop=self.bot.loop)
+		game.current_question_part = 0
+		await self.play_part()
+	
+	# =========================================================================================
 
 	@app_commands.command(name = "開始遊戲")
 	async def start(self, interaction, attachment: discord.Attachment):
 		"""上傳題庫，開始一場猜歌遊戲"""
-			
+		
 		if interaction.guild.id not in self.games:
-			self.games[interaction.guild.id] = GameData()
+			text_channel = self.bot.get_channel(interaction.channel.id)
+			self.games[interaction.guild.id] = GameData(interaction.guild.id, text_channel)
 		game = self.games[interaction.guild.id]
 		
 		if game.step != GameStep.IDLE:
-			await interaction.response.send_message(f"當前伺服器已在 <#{game.channel.id}> 進行遊戲，請先使用 /stop 指令中斷再試", ephemeral=True)
+			await interaction.response.send_message(f"當前伺服器已在 <#{game.channel.id}> 進行遊戲，請先使用 `/結束遊戲` 指令中斷再試", ephemeral=True)
 			return
 		
 		if not interaction.user.voice:
@@ -119,7 +166,16 @@ class SongGuesser(commands.Cog):
 		title = game.question_set["title"]
 		await interaction.response.send_message(f"{interaction.user.name} 開始了 [{title}] 的猜歌遊戲\n加入 <#{game.channel.id}> 頻道一起遊玩吧！")
 		
-		# TODO: 倒數後直接開始第一題
+		#倒數後直接開始第一題
+		message = await game.text_channel.send("遊戲將於 3 秒後開始...")
+		for i in range(2, 0, -1):
+			await asyncio.sleep(1)
+			await message.edit(content=f"遊戲將於 {i} 秒後開始...")
+		await asyncio.sleep(1)
+		await message.edit(content=f"遊戲即將開始...")
+		
+		game.reset_progress()
+		await self.init_question(game)
 		
 	# @app_commands.command()
 	# async def yt(self, interaction, *, url: str):
@@ -151,7 +207,7 @@ class SongGuesser(commands.Cog):
 		game = self.games[interaction.guild.id]
 		await interaction.response.send_message(f"已中斷在 <#{game.channel.id}> 舉行的猜歌遊戲")
 		del self.games[interaction.guild.id]
-			
+		
 	# =========================================================================================
 	
 	async def game_command_pre_check(self, interaction):
@@ -218,8 +274,12 @@ class SongGuesser(commands.Cog):
 			return
 			
 		title = game.question_set["title"]
-		await interaction.response.send_message(f"重新開始了一輪 [{title}] 的猜歌遊戲！\n即將開始第 1 題的播放")
-		# TODO: 重置遊戲進度，倒數 3 秒開始播放第一題
+		await interaction.response.send_message(f"{interaction.user.name} 重新開始了一輪 [{title}] 的猜歌遊戲！\n遊戲即將開始...")
+		# 倒數 3 秒
+		await asyncio.sleep(3)
+		# 重置遊戲進度，開始播放第一題
+		game.reset_progress()
+		await self.init_question(game)
 	
 	# =========================================================================================
 

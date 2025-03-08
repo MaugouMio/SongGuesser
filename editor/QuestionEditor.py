8a8336e5b0694522d58e4f77ccad05918fcfd261
MaugouMio <maugoumine@gmail.com>
2025-03-08 11:48:14 +0800

    QT 串接播放條

--- editor/QuestionEditor.py
+++ editor/QuestionEditor.py
@@ -24,68 +24,65 @@ class QuestionEditor(QMainWindow):
 		self.initUI()
 
 	def initUI(self):
-		self.mediaPlayer = QMediaPlayer()
+		self.media_player = QMediaPlayer()
+		self.media_player.setSource(QUrl.fromLocalFile("../temp/1247388511028514928/main.webm"))
 		self.audioOutput = QAudioOutput()
-		self.mediaPlayer.setAudioOutput(self.audioOutput)
+		self.media_player.setAudioOutput(self.audioOutput)
 
-		self.playButton = QPushButton("Play")
-		self.playButton.clicked.connect(self.playPause)
-		self.frame_audio_player.addWidget(self.playButton)
+		self.play_button.clicked.connect(self.playPause)
 
-		self.positionSlider = Slider(Qt.Orientation.Horizontal)
-		self.positionSlider.setRange(0, 0)
-		self.positionSlider.sliderPressed.connect(self.beginDragPosition)
-		self.positionSlider.sliderMoved.connect(self.setPosition)
-		self.positionSlider.sliderReleased.connect(self.endDragPosition)
-		self.frame_audio_player.addWidget(self.positionSlider)
+		self.position_slider = Slider(Qt.Orientation.Horizontal)
+		self.position_slider.setRange(0, 0)
+		self.position_slider.sliderPressed.connect(self.beginDragPosition)
+		self.position_slider.sliderMoved.connect(self.setPosition)
+		self.position_slider.sliderReleased.connect(self.endDragPosition)
+		self.frame_audio_player.addWidget(self.position_slider)
 
-		self.timeLabel = QLabel("0:00.000 / 0:00.000")
-		self.frame_audio_player.addWidget(self.timeLabel)
-
-		self.mediaPlayer.positionChanged.connect(self.positionChanged)
-		self.mediaPlayer.durationChanged.connect(self.durationChanged)
+		self.media_player.positionChanged.connect(self.positionChanged)
+		self.media_player.durationChanged.connect(self.durationChanged)
 
 		self.setWindowTitle("猜歌機器人題庫編輯器")
 		self.show()
 
 	def playPause(self):
-		if self.mediaPlayer.playbackState() == QMediaPlayer.PlaybackState.PlayingState:
-			self.mediaPlayer.pause()
-			self.playButton.setText("Play")
+		if self.media_player.playbackState() == QMediaPlayer.PlaybackState.PlayingState:
+			self.media_player.pause()
+			self.play_button.setText("▶")
 		else:
-			self.mediaPlayer.setSource(QUrl.fromLocalFile("../temp/1247388511028514928/main.webm"))
-			self.mediaPlayer.play()
-			self.playButton.setText("Pause")
+			self.media_player.play()
+			self.play_button.setText("∎∎")
 
 	def positionChanged(self, position):
-		self.positionSlider.setValue(position)
+		self.position_slider.setValue(position)
 		self.updateTimeLabel()
 
 	def durationChanged(self, duration):
-		self.positionSlider.setRange(0, duration)
-		self.updateTimeLabel()
-	
-	def beginDragPosition(self):
-		self.dragPositionWasPlaying = self.mediaPlayer.playbackState() == QMediaPlayer.PlaybackState.PlayingState
-		self.mediaPlayer.pause()
-
-	def setPosition(self, position):
-		self.mediaPlayer.setPosition(position)
-	
-	def endDragPosition(self):
-		if self.dragPositionWasPlaying:
-			self.mediaPlayer.play()
-
-	def updateTimeLabel(self):
-		position = self.mediaPlayer.position()
-		duration = self.mediaPlayer.duration()
-		position_minutes = position // 60000
-		position_seconds = (position // 1000) % 60
-		position_ms = position % 1000
+		self.position_slider.setRange(0, duration)
+		
 		duration_minutes = duration // 60000
 		duration_seconds = (duration // 1000) % 60
 		duration_ms = duration % 1000
-		self.timeLabel.setText(f"{position_minutes}:{position_seconds:02d}.{position_ms:03d} / {duration_minutes}:{duration_seconds:02d}.{duration_ms:03d}")
+		self.duration_label.setText(f"{duration_minutes}:{duration_seconds:02d}.{duration_ms:03d}")
+		
+		self.updateTimeLabel()
+	
+	def beginDragPosition(self):
+		self.dragPositionWasPlaying = self.media_player.playbackState() == QMediaPlayer.PlaybackState.PlayingState
+		self.media_player.pause()
+
+	def setPosition(self, position):
+		self.media_player.setPosition(position)
+	
+	def endDragPosition(self):
+		if self.dragPositionWasPlaying:
+			self.media_player.play()
+
+	def updateTimeLabel(self):
+		position = self.media_player.position()
+		position_minutes = position // 60000
+		position_seconds = (position // 1000) % 60
+		position_ms = position % 1000
+		self.position_label.setText(f"{position_minutes}:{position_seconds:02d}.{position_ms:03d}")
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
